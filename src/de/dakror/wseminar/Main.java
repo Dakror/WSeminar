979cda8fe88dad8d45bf55578a2d54495f910dbd
Dakror <mail@dakror.de>
2014-11-13 19:20:55 +0100

    Initial commit

---
+++ src/de/dakror/wseminar/Main.java
@@ -0,0 +1,19 @@
+package de.dakror.wseminar;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Dakror
+ */
+public class Main extends Application
+{
+	@Override
+	public void start(Stage primaryStage) throws Exception
+	{}
+	
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+}
