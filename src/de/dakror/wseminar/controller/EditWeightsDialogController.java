8163fe0c250b99be4d47b376a6e88a81364f975c
Dakror <mail@dakror.de>
2015-02-13 19:55:49 +0100

    edit weight list

--- src/de/dakror/wseminar/controller/EditWeightsDialogController.java
+++ src/de/dakror/wseminar/controller/EditWeightsDialogController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.cell.TextFieldListCell;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
@@ -38,6 +39,10 @@ public class EditWeightsDialogController {
 		cancelButton.setOnAction(e -> ((Stage) cancelButton.getScene().getWindow()).close());
 		
 		add.setOnAction(e -> list.getItems().add("Gewicht #" + (list.getItems().size() + 1)));
+		remove.setOnAction(e -> list.getItems().remove(list.getSelectionModel().getSelectedIndex()));
+		
 		list.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> remove.setDisable(newVal == null));
+		list.setCellFactory(TextFieldListCell.forListView());
+		list.setOnEditCommit(t -> list.getItems().set(t.getIndex(), t.getNewValue()));
 	}
 }
